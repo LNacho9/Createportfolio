18b27a1be5d2b60e3ce7f41f33d4a2495d7fdeca
Lnacho9 <ignaciobusto0@gmail.com>
2026-05-13 22:06:57 -0300

    more things

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
+main();
 function main(){
     console.log("Hola mundo");
+}
+function menuportfolio(){
+    const decision_usuario =
+    console.log("------------------HOLA BIENVENIDO A CREATEPORTFOLIO------------------");
+    console.log("aca vas a poder crear tu portfolio y gestionarlo de manera muy simple");
+    console.log("que deseas hacer?");
+    console.log("1-crear nuevo portfolio\n");
+    console.log("2-modificar un portfolio existente\n");
+    console.log("3-salir");
+    
+    
+    
+    
+    
+    
+    
 }
